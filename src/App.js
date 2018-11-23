21297f1da3165b16672ae425f168808de56eb334
bbtora <23010526+bbtora@users.noreply.github.com>
2018-11-23 09:50:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,147 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Particles from 'react-particles-js';
+import Navigation from './Components/Navigation/Navigation';
+import SignIn from './Components/SignIn/SignIn';
+import Register from './Components/Register/Register';
+import Logo from './Components/Logo/Logo';
+import ImageUrlBar from './Components/ImageUrlBar/ImageUrlBar';
+import Rank from './Components/Rank/Rank';
+import FaceRecog from './Components/FaceRecog/FaceRecog';
+
+
+const particlesOptions = {
+    particles: {
+      number: {
+        value: 30,
+        density: {
+          enable: true,
+          value_area: 800,
+        }
+      }
+    }
+}
+
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: "",
+    email: "",
+    entries: 0,
+    joined: ''
+  }
+};
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = initialState;
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
+  }
+  
+  calcFaceLocation = (data) => {
+    const faceData = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = image.width;
+    const height = image.height;
+    return {
+      leftCol: faceData.left_col * width,
+      topRow: faceData.top_row * height,
+      rightCol: width - (faceData.right_col * width),
+      bottomRow: height - (faceData.bottom_row * height),
+    }
+  }
+
+  displayBox = (box) => {
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState(initialState);
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true});
+    }
+    this.setState({route: route});
+  }
+
+  onPictureSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    fetch('http://localhost:3000/imageurl', {
+      method: "post",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+    .then(response => {
+      if (response) {
+        fetch('http://localhost:3000/image', {
+          method: "put",
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+        .then(resp => resp.json())
+        .then(count => {
+          this.setState(Object.assign(this.state.user, {entries: count}))
+        })
+        .catch(console.log);
+      }
+      this.displayBox(this.calcFaceLocation(response))
+    })
+    .catch(err => console.log(err));
+  }
+
   render() {
+    const { imageUrl, box, route, isSignedIn} = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Navigation 
+          onRouteChange={this.onRouteChange}
+          isSignedIn={isSignedIn}  
+        />
+        { route === 'home'
+          ? <div>
+              <Logo />
+              <Rank name={this.state.user.name} entries={this.state.user.entries} />
+              <ImageUrlBar 
+                onInputChange={this.onInputChange}
+                onPictureSubmit={this.onPictureSubmit}  
+              />
+              <FaceRecog 
+                imageUrl={imageUrl}
+                box={box}
+              />
+            </div> 
+          : (
+            route === 'signin'
+              ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+            )
+        }
       </div>
     );
   }
